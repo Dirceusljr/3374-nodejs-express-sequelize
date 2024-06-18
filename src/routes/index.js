e27ae95c8e662d08f12faff95a3bcca92f0d54c3
Dirceusljr <dirceusljr@gmail.com>
2024-06-18 14:40:49 -0300

    criando rotas de categoria e curso e adicionando criar matricula na rota pessoas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const pessoas = require('../routes/pessoaRoute.js');
+const categorias = require('../routes/categoriaRoute.js');
+const cursos = require('../routes/cursoRoute.js');
 
 const router = app => {
   app.use(
     express.json(),
-    pessoas
+    pessoas,
+    categorias,
+    cursos
   );
 };
 
