fa1991cc180caff6c524fdab657756f806e48fc2
Dirceusljr <dirceusljr@gmail.com>
2024-06-18 16:56:50 -0300

    projeto final curso 01

--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -8,6 +8,16 @@ class PessoaController extends Controller{
   constructor() {
     super(pessoaServices);
   }
+
+  async pegaMatriculas(req, res) {
+    const { estudanteId } = req.params;
+    try {
+      const listaMatriculadas = await pessoaServices.pegaMatriculasPorEstudante(Number(estudanteId));
+      return res.status(200).json(listaMatriculadas);
+    } catch (erro) {
+      //erro
+    }
+  }
 }
 
 module.exports = PessoaController;
--- src/routes/pessoaRoute.js
+++ src/routes/pessoaRoute.js
@@ -11,6 +11,7 @@ router.get('/pessoas/:id', (req, res) => pessoaController.pegaUmPorID(req, res))
 router.post('/pessoas', (req, res) => pessoaController.criaNovo(req, res));
 router.put('/pessoas/:id', (req, res) => pessoaController.atualiza(req, res));
 router.delete('/pessoas/:id', (req, res) => pessoaController.exclui(req, res));
-router.post('/pessoas/:estudanteID/matriculas', (req, res) => matriculaController.criaNovo(req, res));
+router.get('/pessoas/:estudanteId/matriculas', (req, res) => pessoaController.pegaMatriculas(req, res));
+router.post('/pessoas/:estudanteId/matriculas', (req, res) => matriculaController.criaNovo(req, res));
 
 module.exports = router;
